10b594a3235cc569512d8d8c51951f2ffd8811ae
Aurynn Shaw <self@aurynn.com>
2013-11-02 16:26:40 +1300

    Module now working

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,22 @@
 class rainy (
     $admin_password,
-    $ssl         => true,
-    $ssl_cert    => "",
-    $interface   => "*",
-    $port        => 8080,
-    $datapath    => "/var/lib/rainy/data",
-    $installpath => "/opt/rainy/",
-    $backend     => "sqlite",
-    $version     => "0.5.0"
+    $ssl         = true,
+    $ssl_cert    = "",
+    $interface   = "*",
+    $port        = 8080,
+    $datapath    = "/var/lib/rainy/data/",
+    $installpath = "/opt/rainy",
+    $backend     = "sqlite",
+    $version     = "0.5.0"
 ) {
 
     # Needs to be set up before most of everything 
     # else.
-    $packages = ["mono-complete","libsqlite3-0", "unzip"]
+    $packages = ["mono-complete","libsqlite3-0", "unzip", "curl"]
 
     $installed = "$installpath/rainy-$version"
+    $configfile = "/etc/rainy/settings.conf"
+    $piddir    = "/var/run/rainy"
 
     package {$packages:
         ensure => present,
@@ -28,26 +30,31 @@ class rainy (
     # Now,
     # Let's download the file.
     exec {"fetch rainy archive":
-        command => "/usr/bin/curl http://rainy.notesync.org/release/rainy-$verision.zip > /tmp/rainy.zip",
+        command => "/usr/bin/curl http://rainy.notesync.org/release/rainy-$version.zip > /tmp/rainy.zip",
         creates => "/tmp/rainy.zip"
     }
     exec {"unzip rainy archive":
-        command => "/usr/bin/unzip",
-        creates => "$installpath/rainy-$version",
-        requires => [
+        command => "/usr/bin/unzip /tmp/rainy.zip -d $installpath",
+        creates => "$installed/Rainy.exe",
+        require => [
             Package["unzip"], 
             File[$installpath]
         ]
     }
 
     exec {"make $datapath":
-        command => "/usr/bin/mkdir -p $datapath",
+        command => "/bin/mkdir -p $datapath",
         creates => "$datapath"
     }
 
     file {$installpath: 
         ensure => directory,
         owner  => "rainy"
+    }
+    file {$installed:
+        ensure  => directory,
+        owner   => "rainy",
+        require => Exec["unzip rainy archive"]
     }
 
     file {$datapath:
@@ -64,7 +71,7 @@ class rainy (
         owner  => "rainy"
     }
 
-    file {"/etc/rainy/settings.conf":
+    file {$configfile:
         ensure  => present,
         content => template("rainy/settings.conf.erb"),
         owner   => "rainy",
@@ -73,26 +80,34 @@ class rainy (
 
     file {"/etc/init.d/rainy":
         ensure  => present,
-        content => template("rainy/etc/init.d/rainy.erb")
+        content => template("rainy/etc/init.d/rainy.erb"),
+        mode    => "0755"
     }
 
-    file {"/var/run/rainy/":
+    file {$piddir:
         ensure => directory,
         owner  => "rainy"
     }
-    file {"/etc/default/rainy":
-        ensure => present,
-        content => template("rainy/etc/default/rainy")
+    file {"$installed/rainy.sh":
+        ensure  => present,
+        content => template("rainy/rainy.sh.erb"),
+        require => File[$installed],
+        mode    => "0755"
+    }
+    file {"/home/rainy":
+        ensure => directory,
+        owner  => "rainy"
     }
 
     service {"rainy":
         ensure => running,
         require => [
-            File["/etc/rainy/settings.conf"],
+            File[$configfile],
             File["/etc/init.d/rainy"],
             Exec["unzip rainy archive"],
-            File["/var/run/rainy"],
-            File["/etc/default/rainy"]
+            File[$piddir],
+            File["$installed/rainy.sh"]
+            File["/home/rainy"]
         ]
     }
 
